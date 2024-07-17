44eb339cb49e3442702f1c8fdccc4b69a589a241
ArecaGilll <163608523+ArecaGilll@users.noreply.github.com>
2024-07-17 20:52:12 +0200

    Update main.js

--- change-colors-with-local-storage/main.js
+++ change-colors-with-local-storage/main.js
@@ -13,5 +13,5 @@ for (var i = 0; i < el.length; i++) {
             document.body.classList.add(this.getAttribute("data-color"));
 .
             localStorage.setItem("pageColor",this.getAttribute("data-color"));
-    }, false);
+    }, false);.
 }
